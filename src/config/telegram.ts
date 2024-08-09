2bf57722c8164f87fbccaad93288c3d86d9dfc75
indianapoly <hyunrim03@naver.com>
2024-08-09 09:22:11 +0900

    fix: 실행안되는 문제 해결

--- src/config/telegram.ts
+++ src/config/telegram.ts
@@ -1,6 +1,6 @@
 /** @format */
 import * as dotenv from 'dotenv';
-import TelegramBot = require('node-telegram-bot-api');
+import TelegramBot from 'node-telegram-bot-api';
 dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN as string;
