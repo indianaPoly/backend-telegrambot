5de3651fcc3cfddce92d4adeb90722f2c26d8fb0
george <hyunrim03@naver.com>
2024-08-10 14:56:27 +0900

    chore: pretiier 설정에 따른 수정

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,11 @@
 /** @format */
-import { help, insight, start } from './command/index';
+import { help, insight, start } from "./command/index";
 
 const main = () => {
-    console.log('서버가 실행되었습니다.');
-    start();
-    help();
-    insight();
+  console.log("서버가 실행되었습니다.");
+  start();
+  help();
+  insight();
 };
 
 main();
--- src/command/start.ts
+++ src/command/start.ts
@@ -1,10 +1,13 @@
 /** @format */
-import { bot } from '../config/index.js';
+import { bot } from "../config/index.js";
 
 export const start = () => {
-    bot.onText(/\/start/, (msg) => {
-        const chatID = msg.chat.id;
-        bot.sendMessage(chatID, '안녕하세요. 취업 정보 제공 봇입니다.');
-        bot.sendMessage(chatID, '각종 명령어를 확인하고 싶으시면 \n/도움말을 입력해주세요.');
-    });
+  bot.onText(/\/start/, (msg) => {
+    const chatID = msg.chat.id;
+    bot.sendMessage(chatID, "안녕하세요. 취업 정보 제공 봇입니다.");
+    bot.sendMessage(
+      chatID,
+      "각종 명령어를 확인하고 싶으시면 \n/도움말을 입력해주세요."
+    );
+  });
 };
