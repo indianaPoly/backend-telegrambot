5e579e01eb6836893f611d50733b8d3f4eb64506
george <hyunrim03@naver.com>
2024-08-17 15:59:38 +0900

    feat: category recommand_ 에 대한 callback 처리

--- src/command/resume/recommand/handler/callbackHandler.ts
+++ src/command/resume/recommand/handler/callbackHandler.ts
@@ -1,18 +1,27 @@
 import { bot } from '../../../../config/index';
 import { getLinksByCategory } from '../keyboards/linksKeyboard';
+import { changeName } from '../../../../util/index';
 
 export const handleCallbackQuery = (callbackQuery: any) => {
   const chatId = callbackQuery.message.chat.id;
   const category = callbackQuery.data;
   const links = getLinksByCategory(category);
 
-  if (links.length > 0) {
-    links.forEach(link => {
-      bot.sendMessage(chatId, link.text, {
-        reply_markup: {
-          inline_keyboard: [[{ text: link.text, url: link.url }]]
-        }
-      });
-    });
+  const value = links.map((item) => {
+    return [{ text: item.text, url: item.url }];
+  });
+
+  const options = {
+    reply_markup: {
+      inline_keyboard: value,
+    },
+  };
+
+  if (category.startsWith('recommand_')) {
+    bot.sendMessage(
+      chatId,
+      `${changeName(category)}에 맞는 사이트를 추천해드릴게요!`,
+      options
+    );
   }
 };
