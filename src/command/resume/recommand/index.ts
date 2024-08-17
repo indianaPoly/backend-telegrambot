9cb060a10e95d18f13ef858668680723fae8a2ab
george <hyunrim03@naver.com>
2024-08-17 16:00:16 +0900

    update

--- src/command/resume/recommand/index.ts
+++ src/command/resume/recommand/index.ts
@@ -3,11 +3,11 @@ import { getCategoryKeyboard } from './keyboards/categoryKeyboard';
 import { handleCallbackQuery } from './handler/callbackHandler';
 
 export const handleRecommand = (msg: any) => {
-  const responseText = '자소서와 관련된 정보를 제공해드려요! 카테고리를 선택해주세요.';
+  const responseText =
+    '자소서와 관련된 정보를 제공해드려요! 카테고리를 선택해주세요.';
   bot.sendMessage(msg.chat.id, responseText, getCategoryKeyboard());
 };
 
-// 이 부분은 bot.on('callback_query') 핸들러에 추가해야 합니다.
 bot.on('callback_query', (callbackQuery) => {
   handleCallbackQuery(callbackQuery);
 });
