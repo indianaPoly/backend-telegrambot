760eca3bf80baa91386faccbb37b1c6e67cf43a8
george <hyunrim03@naver.com>
2024-08-17 15:59:59 +0900

    refactoring: 파일 분리 진행

--- src/command/resume/article/index.ts
+++ src/command/resume/article/index.ts
@@ -1,17 +1,7 @@
 import { bot } from '../../../config/index';
-
-export const getArticlesKeyboard = () => {
-  return {
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Article 1', url: 'https://www.examplearticle.com' }],
-        [{ text: 'Article 2', url: 'https://www.anotherarticle.com' }],
-      ],
-    },
-  };
-};
+import { getArticlesKeyboard } from './keyboards/articleKeyboard';
 
 export const handleArticles = (msg: any) => {
-  const responseText = '자기소개서 관련 아티클 목록입니다:';
-  bot.sendMessage(msg.chat.id, responseText, getArticlesKeyboard());
+  const responseText = '자기소개서 관련 아티클 목록입니다';
+  bot.sendMessage(msg.chat.id, responseText, getArticlesKeyboard);
 };
---
+++ src/command/resume/article/keyboards/articleKeyboard.ts
@@ -0,0 +1,8 @@
+export const getArticlesKeyboard = {
+  reply_markup: {
+    inline_keyboard: [
+      [{ text: 'Article 1', url: 'https://www.examplearticle.com' }],
+      [{ text: 'Article 2', url: 'https://www.anotherarticle.com' }],
+    ],
+  },
+};
