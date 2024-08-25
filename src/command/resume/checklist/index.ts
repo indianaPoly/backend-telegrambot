db82438b05ff9c7fe1df1802ed01c5e56a3d95d6
indianaPoly <hyunrim03@naver.com>
2024-08-25 14:05:56 +0900

    update: 1초 이후에 보이도록 수정

--- src/command/resume/checklist/index.ts
+++ src/command/resume/checklist/index.ts
@@ -1,7 +1,12 @@
 import { bot } from '../../../config/index';
 import { Message } from '../../../types/index';
 
-import { areAllItemsChecked, getChecklistKeyboard, resetCheckListItems, toggleChecklistItem } from './util';
+import {
+  areAllItemsChecked,
+  getChecklistKeyboard,
+  resetCheckListItems,
+  toggleChecklistItem,
+} from './util';
 
 export const handleChecklist = (msg: Message) => {
   bot.sendMessage(msg.chat.id, '최종 점검 리스트', getChecklistKeyboard());
@@ -17,7 +22,9 @@ export const handleChecklistToggle = (msg: Message, itemId: number) => {
   });
 
   if (areAllItemsChecked()) {
-    bot.sendMessage(msg.chat.id, '자소서를 제출해도 좋습니다!');
-    resetCheckListItems();
+    setTimeout(() => {
+      bot.sendMessage(msg.chat.id, '자소서를 제출해도 좋습니다!');
+      resetCheckListItems();
+    }, 1000);
   }
 };
