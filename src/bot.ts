a84e6a7ac3e0ec358eb9585f6e5bfa8bdcd37b80
indianaPoly <hyunrim03@naver.com>
2024-08-25 14:03:04 +0900

    add: 함수 추가

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,11 @@
 /** @format */
-import { help, insight, resume, start } from './command/index';
+import { help, insight, recruiting, resume, start } from './command/index';
 
 const main = () => {
   console.log('서버가 실행되었습니다.');
   start();
   help();
+  recruiting();
   insight();
   resume();
 };
